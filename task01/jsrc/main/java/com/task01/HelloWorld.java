a981e5f3a5a82b0f7ffe37995b4499e0ce326f7c
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2025-02-17 11:40:20 +0200

    modified 3 lambda

--- task01/jsrc/main/java/com/task01/HelloWorld.java
+++ task01/jsrc/main/java/com/task01/HelloWorld.java
@@ -12,7 +12,6 @@ import java.util.Map;
     lambdaName = "hello_world",
 	roleName = "hello_world-role",
 	isPublishVersion = false,
-	aliasName = "${lambdas_alias_name: learn}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
